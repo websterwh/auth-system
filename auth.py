7e63a427442182f91b87bd46136a5a1b6fb61316
William W <81191122+websterwh@users.noreply.github.com>
2022-10-20 15:30:49 -0400

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,8 @@
 import time
 import requests
 from dhooks import Webhook, Embed
-webhook = 'https://discord.com/api/webhooks/941192613321654292/I_h8uR1g7oOakwtKaC7spUl0gawQ1gE8kZIxF4P4TxRAB5nZGvgnL2B7-QoSfVNZiZ82'
-#token = '6eb26e56-1a02-41ec-891e-d3d2d1c87b3d'
-link = 'https://pastebin.com/raw/pxdyiiD9'
+webhook = ''
+link = ''
 auth = requests.get(link).text
 hook = Webhook(webhook)
 t = time.localtime()
